da029984effd82ea1bfbb9667635fc4b74e7e73e
nathanhleung <github@nathanhleung.com>
2022-01-13 09:24:18 -0800

    Add team section

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,14 @@
-import { Box, Center, Flex, Heading, Link, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Center,
+  Flex,
+  Grid,
+  GridItem,
+  Heading,
+  Image,
+  Link,
+  Text,
+} from "@chakra-ui/react";
 import { ethers } from "ethers";
 import { useEffect, useState } from "react";
 import { Helmet } from "react-helmet";
@@ -52,7 +62,7 @@ function App() {
       <Helmet>
         <title>NFSheets</title>
       </Helmet>
-      <Box w="100%" h="100%" bg="gray.100">
+      <Box w="100%" minHeight="100%" bg="gray.100">
         <NavBar />
         <Flex w="100%" mt={3} mb={6} flexWrap="wrap">
           <Box flex={1} mr={3} minWidth={["100%", "unset"]} p={3}>
@@ -72,7 +82,7 @@ function App() {
             <CellEditor width="100%" />
           </Box>
         </Flex>
-        <Center p={6} pb={10} textAlign="center">
+        <Center p={6} textAlign="center">
           <Box maxWidth={["100%", "50%"]}>
             <Heading pb={4}>About</Heading>
             <Text textAlign="left">
@@ -91,6 +101,62 @@ function App() {
               </Link>
               .
             </Text>
+          </Box>
+        </Center>
+        <Center p={6} pb={12} textAlign="center">
+          <Box maxWidth={["100%", "50%"]}>
+            <Heading pb={4}>Team</Heading>
+            <Text pb={5}>
+              We are a team of students from Penn State and UCLA.
+            </Text>
+            <Grid templateColumns="repeat(4, 1fr)" gap={5}>
+              <GridItem>
+                <Image
+                  borderRadius="full"
+                  boxSize="150px"
+                  src="https://media-exp1.licdn.com/dms/image/C5603AQErWTFdOBbYLQ/profile-displayphoto-shrink_800_800/0/1605148857873?e=1647475200&v=beta&t=pwRYHtBtRs4rZ2In6CNiMMbFbsUwbcy_eKCpPxR-m3M"
+                />
+                <Heading size="sm" mt={5}>
+                  Ben Hoyer
+                </Heading>
+              </GridItem>{" "}
+              <GridItem>
+                <Image
+                  borderRadius="full"
+                  boxSize="150px"
+                  src="https://pbs.twimg.com/profile_images/1415495752617533441/2Vdj34wy_400x400.jpg"
+                />
+                <Heading size="sm" mt={5}>
+                  Matt Maiale
+                </Heading>
+                <Text>
+                  <Link href="https://www.maiale.me">maiale.me</Link>
+                </Text>
+              </GridItem>
+              <GridItem>
+                <Image
+                  borderRadius="full"
+                  boxSize="150px"
+                  src="https://avatars.githubusercontent.com/u/10874225?v=4"
+                />
+                <Heading size="sm" mt={5}>
+                  Nathan Leung
+                </Heading>
+                <Text>
+                  <Link href="https://www.natecation.com">natecation.com</Link>
+                </Text>
+              </GridItem>
+              <GridItem>
+                <Image
+                  borderRadius="full"
+                  boxSize="150px"
+                  src="https://avatars.githubusercontent.com/u/25235154?v=4"
+                />
+                <Heading size="sm" mt={5}>
+                  Bryce Miles
+                </Heading>
+              </GridItem>
+            </Grid>
           </Box>
         </Center>
       </Box>
